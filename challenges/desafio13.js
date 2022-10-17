6c4106c148b618336d3a931ad85267b16efdd6e8
Fabiane Malaquias <malaquiasfr@gmail.com>
2022-10-17 16:29:37 -0300

    Desafio 13 - Inclua o campo criadoPor em todos os documentos, colocando Ronald McDonald no valor desse campo.

---
+++ challenges/desafio13.js
@@ -0,0 +1,14 @@
+// 13 - Inclua o campo criadoPor em todos os documentos, colocando Ronald McDonald no valor desse campo.
+db.produtos
+  .updateMany(
+    {},
+    { $set: { criadoPor: "Ronald McDonald" } },
+);
+
+db.produtos.find(
+  {}, 
+  { _id: 0, nome: 1, criadoPor: 1 },
+);
+
+// set: altera valor do campo, se não existir, 
+// ADICIONA novo campo com valor especificado.
