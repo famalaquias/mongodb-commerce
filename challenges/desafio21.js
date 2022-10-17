3f91905d0e96950f646bd4bf60d35756af0cc7d1
Fabiane Malaquias <malaquiasfr@gmail.com>
2022-10-17 17:59:30 -0300

    Desafio 21 - Remova o último ingrediente do sanduíche Cheddar McMelt.

---
+++ challenges/desafio21.js
@@ -0,0 +1,11 @@
+// 21 - Remova o último ingrediente do sanduíche Cheddar McMelt.
+db.produtos
+.updateOne(
+  { nome: "Cheddar McMelt" },
+  { $pop: { ingredientes: 1 } }, // 1 remove o último item
+);
+
+db.produtos.find(
+{}, 
+{ _id: 0, nome: 1, ingredientes: 1 },
+);
