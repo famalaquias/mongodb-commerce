577fd8f74c771cc0c679e6a743ccc103370f3884
Fabiane Malaquias <malaquiasfr@gmail.com>
2022-10-17 19:03:16 -0300

    Desafio 29 - Renomeie o campo descricao para descricaoSite em todos os documentos.

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,4 +1,4 @@
 // 28 - Conte quantos produtos têm 4 ingredientes.
 db.produtos.countDocuments(
-  { ingredientes: { $size: 4 } } // size: filtrar documentos pelo tamanho de arrays. 
+  { ingredientes: { $size: 4 } }, // size: filtrar documentos pelo tamanho de arrays. 
 );
